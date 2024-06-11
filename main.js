35b40c4f92fc37735b7844b77fc76b2518947298
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-06-12 01:01:52 +0300

    search-works

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
+let searchResults;
+
 function FindBook()
 {
+    searchResults = document.getElementById("list");
     const searchInput = document.getElementById("main-search");
-    const searchResults = document.getElementById("list");
-    console.log(2);
     const searchItem = searchInput.value;
-    console.log(searchItem);
     if (searchItem.trim() === ""){
        searchResults.innerHTML = ""; 
     }
-    //const apiUrlBooks = 'https://openlibrary.org/search.json?format=json&action=query&list=search&utf8=1&srsearch=${searchItem}';
-    const apiUrlBooks = `https://openlibrary.org/search.json?q=${searchItem}&fields=*,availability&limit=1`;
+    const apiUrlBooks = `https://openlibrary.org/search.json?q=${searchItem}&fields=*,availability&limit=10`;
     
     fetch(apiUrlBooks)
     .then(response => {
         if (!response.ok) {
-            if (response.status === 404) {
-                throw new Error('Data not found');
-              } else if (response.status === 500) {
-                throw new Error('Server error');
-              } else {
-                throw new Error('Network response was not ok');
-              }
+            throw new Error('Network response was not ok');
         }
         return response.json();
     })
     .then(data => {
-        //searchResults.innerHTML= JSON.stringify(data, null, 2);
         console.log(data);
-        const results = data.query.search;
+        const results = data.docs;
         ShowResults(results);
     })
     .catch(error => {
@@ -35,11 +27,14 @@ function FindBook()
     }); 
 }
 function ShowResults(results){
+    searchResults = document.getElementById("list");
     searchResults.innerHTML='';
     results.forEach(result => {
         const listBook = document.createElement("li");
         const author = document.createElement("p");
-        listBook.textContent = result.title;
-        author.textContent = result.author;
+        listBook.textContent = '"' + result.title + '"';
+        author.textContent = 'by ' + result.author_name;
+        listBook.appendChild(author);
+        searchResults.appendChild(listBook);
     });
 }
